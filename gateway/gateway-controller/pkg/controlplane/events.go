1115119e2fe0267e9c237fc0e54d701f6f184fbc
Malintha Amarasinghe <malintha.prasan@gmail.com>
2025-10-22 12:58:30 +0530

    Implement WebSocket client with connection lifecycle and reconnection logic

---
+++ gateway/gateway-controller/pkg/controlplane/events.go
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package controlplane
+
+// ConnectionAckMessage represents the acknowledgment message sent by the control plane
+// upon successful WebSocket connection establishment
+type ConnectionAckMessage struct {
+	Type         string `json:"type"`         // Message type (always "connection.ack")
+	GatewayID    string `json:"gatewayId"`    // Gateway UUID
+	ConnectionID string `json:"connectionId"` // Unique connection UUID
+	Timestamp    string `json:"timestamp"`    // RFC3339 timestamp
+}
