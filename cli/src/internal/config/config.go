3a0a5b97b69da97c7b503a3a04f93c951b60989a
piyumaldk <piyumaldk@gmail.com>
2025-12-16 12:30:06 +0530

    Change config to owner only

--- cli/src/internal/config/config.go
+++ cli/src/internal/config/config.go
@@ -115,7 +115,7 @@ func SaveConfig(config *Config) error {
 		return fmt.Errorf("failed to marshal config: %w", err)
 	}
 
-	if err := os.WriteFile(configPath, data, 0644); err != nil {
+	if err := os.WriteFile(configPath, data, 0600); err != nil {
 		return fmt.Errorf("failed to write config file: %w", err)
 	}
 
