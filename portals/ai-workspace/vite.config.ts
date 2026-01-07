fdca731d31839f3e127e6afa5060d39b02ecaca2
arshardh <arshardhifthikar50@gmail.com>
2026-01-07 13:44:47 +0530

    Add basic AI workspace

---
+++ portals/ai-workspace/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+      ignored: ['**/node_modules/**', '**/.git/**'],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents'],
+  },
+})
+
+
