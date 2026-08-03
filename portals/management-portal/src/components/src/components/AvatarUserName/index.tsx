387448bf5854d6b69cf82ebfa2cce6531f1ae144
Lasantha Samarakoon <lasanthas@wso2.com>
2026-08-03 09:13:33 +0530

    Remove management-portal ahead of Oxygen UI console migration

--- portals/management-portal/src/components/src/components/AvatarUserName/index.tsx
+++
@@ -1,1 +0,0 @@
-export { AvatarUserName } from './AvatarUserName';
--- portals/management-portal/src/pages/Admin.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Typography } from "@mui/material";
-const Admin: React.FC = () => <Typography variant="h5">Admin</Typography>;
-export default Admin;
-
