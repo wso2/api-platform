219e5147277648eb32cb4e295bf47038a58bc6d4
dushaniw <dushaniw@wso2.com>
2026-09-21 01:50:10 +0530

    apip-cloud-ui-managed-portals: align name.ts header, broaden too-short message
    Uses the proprietary WSO2 header that the rest of the package carries,
    and calls out that the min-length applies to the derived handle rather
    than the raw name so hyphens are not implicitly excluded.

--- portals/cloud-plugins/apip-cloud-ui-managed-portals/src/utils/name.ts
+++ portals/cloud-plugins/apip-cloud-ui-managed-portals/src/utils/name.ts
@@ -1,19 +1,10 @@
 /*
- * Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2026, WSO2 LLC (http://www.wso2.com). All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * This software is the property of WSO2 LLC and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
 /**
@@ -64,7 +55,7 @@ export function validatePortalName(name: string): string | undefined {
     return 'Include at least one letter or number.';
   }
   if (handle.length < MIN_PORTAL_HANDLE_LENGTH) {
-    return `The derived handle "${handle}" is too short; use at least ${MIN_PORTAL_HANDLE_LENGTH} letters or digits.`;
+    return `The derived handle "${handle}" is too short; use at least ${MIN_PORTAL_HANDLE_LENGTH} characters.`;
   }
   if (handle.length > MAX_PORTAL_HANDLE_LENGTH) {
     return `The derived handle "${handle}" exceeds ${MAX_PORTAL_HANDLE_LENGTH} characters; shorten the name.`;
